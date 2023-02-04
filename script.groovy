96b3cf8c191bfef61a452454642f5f9eb49bce11
EzeChinedumUchenna <ezechinedum504@gmail.com>
2023-02-04 16:08:54 +0100

    update buildimage

--- script.groovy
+++ script.groovy
@@ -13,9 +13,9 @@
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-ub-credentials', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+        sh 'docker build -t nedumdocker/maven-java-nana:jma-2.0 .'
         sh "echo $PWD | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push nedumdocker/maven-java-nana:jma-2.0'
     }
 } 
 
