3a0e2f75a1e34e651282b55e489f3a644445c65a
Eze Chinedum <102483586+EzeChinedumUchenna@users.noreply.github.com>
2023-02-10 07:52:17 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,22 +1,8 @@
- def buildJar() {
-    echo "Building the application in .jar."
-    sh 'mvn package'
-} 
-
- def buildApp() {
+def buildApp() {
     echo "building"
     //gv.buildJar()
     def test = 1 + 5 > 3 ? 'yes' : 'Not at all'
     echo test
-} 
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-ub-credentials', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
-        sh 'docker build -t nedumdocker/maven-java-nana:jma-2.0 .'
-        sh "echo $PWD | docker login -u $USER --password-stdin"
-        sh 'docker push nedumdocker/maven-java-nana:jma-2.0'
-    }
 } 
 
 def testApp() {
